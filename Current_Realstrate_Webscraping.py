5014f2417793cd00267ff1cef9313f8128ab7cbe
Narasimha lakamsani <nc.lakamsani@gmail.com>
2020-10-23 17:29:12 -0400

    Update Current_Realstrate_Webscraping.py

--- Current_Realstrate_Webscraping.py
+++ Current_Realstrate_Webscraping.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas
 
+# Get contents from the century21 website
 r = requests.get("https://www.century21.com/real-estate/dallas-tx/LCTXDALLAS/", headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
 c = r.content
 
